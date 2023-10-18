4aba1b77a5ac6dd607aa800c3ade3244005146bc
Chen <alchen@bdo.ca>
2023-10-17 20:21:47 -0400

    testscript

--- import pandas as pd.py
+++ import pandas as pd.py
@@ -1,12 +1,6 @@
-import streamlit as st
 import pandas as pd
-import numpy as np
-import pydeck as pdk
 import requests
 
-st.title("Bikespace Dashboard")
-
-# https://api-dev.bikespace.ca/api/v2/docs 
 def get_submission_data():
     url = 'https://api-dev.bikespace.ca/api/v2/submissions'
     headers = {'accept': 'application/json'}
@@ -35,31 +29,7 @@ def submissions_to_dataframe(submissions):
 
 # Fetch data and convert to DataFrame
 submissions_data = get_submission_data()
-chart_data = submissions_to_dataframe(submissions_data)
+df = submissions_to_dataframe(submissions_data)
 
-
-st.pydeck_chart(pdk.Deck(
-    map_style=None,
-    initial_view_state=pdk.ViewState(
-        latitude=43.651070,
-        longitude=-79.347015,
-        zoom=11,
-        pitch=40.5,
-        bearing=-27.36,
-    ),
-    layers=[
-        pdk.Layer(
-            'HexagonLayer',
-            data=chart_data,
-            get_position='[longitude, latitude]',
-            get_color='[200, 30, 0, 160]',
-            get_radius=200,
-            auto_highlight=True,
-            elevation_scale=50,
-            pickable=True,
-            elevation_range=[0, 3000],
-            extruded=True,
-            coverage=1,
-        ),
-    ],
-))
+# Now, 'df' is your Pandas DataFrame containing the submission data.
+print(df)
